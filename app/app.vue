36c35e67e2baa587a61b4e91ad58416b54dce940
coller <collerhu@gmail.com>
2026-07-08 14:01:40 +0800

    修复标题不显示的问题

--- app/app.vue
+++ app/app.vue
@@ -9,6 +9,15 @@
 
 <script setup lang="ts">
 const { fetchSettings } = useSettings()
+const route = useRoute()
 
 useAsyncData('global-settings', () => fetchSettings())
+
+useHead(() => {
+  const title = route.meta.title || 'APayShop'
+  return {
+    title: title,
+    titleTemplate: title === 'APayShop' ? '%s' : '%s - APayShop'
+  }
+})
 </script>
